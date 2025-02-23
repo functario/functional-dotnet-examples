15b069e2ba71131856950ccf0d7eaf156fd32bc1
Functario <166994080+functario@users.noreply.github.com>
2025-02-23 10:19:39 -0500

    Create sql via Aspire

---
+++ 1_Object_Oriented/src/Commons/WellKnowns/Infrastructure/AlexandriaSqldb/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace WellKnowns.Infrastructure.AlexandriaSqldb;
+
+public static class Constants
+{
+    public const int SQLLocalDefaultPort = 50925;
+    public const string SQLAdminId = "sa";
+    public const string SQLDbName = "alexandria";
+    public const string SQLDbLocalSAPassword = "Password1234!";
+    public const string SQLDbLocalIP = "127.0.0.1";
+}
---
+++ 1_Object_Oriented/src/Commons/WellKnowns/Infrastructure/AlexandriaSqldb/ProjectReferences.cs
@@ -0,0 +1,6 @@
+﻿namespace WellKnowns.Infrastructure.AlexandriaSqldb;
+
+public static class ProjectReferences
+{
+    public const string ProjectName = "alexandria-sqldb";
+}
