b6fe90c0af8a735c7de2d929ed2e8cc798c7a2a8
Functario <166994080+functario@users.noreply.github.com>
2025-03-15 15:57:20 -0400

    Cleanup

--- 1_Object_Oriented/test/IntegrationTests/Alexandria.Local.IntegrationTests/GlobalUsing.cs
+++ 1_Object_Oriented/test/IntegrationTests/Alexandria.Local.IntegrationTests/GlobalUsing.cs
@@ -1,7 +1,4 @@
-﻿//global using NSubstitute;
-
-global using static Alexandria.Local.IntegrationTests.Support.KiotaExtensions;
-global using Aspire.Hosting.ApplicationModel;
+﻿global using Aspire.Hosting.ApplicationModel;
 global using Aspire.Hosting.Testing;
-//global using System.Net;
 global using Microsoft.Extensions.DependencyInjection;
+global using static Alexandria.Local.IntegrationTests.Support.KiotaExtensions;
