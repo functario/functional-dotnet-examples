d1ba78e1c18a8723b86e37054cc73ee8320cba70
functario <166994080+functario@users.noreply.github.com>
2025-03-28 10:43:55 -0400

    refactor

--- 1_Object_Oriented/src/Infrastructure/Alexandria.Persistence/Configurations/Interceptors/OnBookModelCreatedInterceptor.cs
+++ 1_Object_Oriented/src/Infrastructure/Alexandria.Persistence/Configurations/Interceptors/OnBookModelCreatedInterceptor.cs
@@ -1,4 +1,5 @@
-﻿using Alexandria.Domain.AuthorDomain;
+﻿using System.Threading;
+using Alexandria.Domain.AuthorDomain;
 using Alexandria.Persistence.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
@@ -25,26 +26,7 @@ internal class OnBookModelCreatedInterceptor : SaveChangesInterceptor
         foreach (var entry in entries)
         {
             var book = entry.Entity;
-            var missingAuthorIds = new List<long>();
-            foreach (var authorId in book.Publication.AuthorsIds)
-            {
-                var author = await dbContext.FindAsync<AuthorModel>([authorId], cancellationToken);
-                if (author is null)
-                {
-                    missingAuthorIds.Add(authorId);
-                    continue;
-                }
-
-                // Join Author and Authors via AuthorsBooks join table.
-                book.Authors.Add(author);
-            }
-
-            if (missingAuthorIds.Count > 0)
-            {
-                throw new InvalidOperationException(
-                    $"The {nameof(Author)}s with following Ids do not exist: [{string.Join(';', missingAuthorIds)}]"
-                );
-            }
+            await MapAuthorsAsync(dbContext, book, cancellationToken);
         }
 
         return await base.SavingChangesAsync(
@@ -52,5 +34,33 @@ internal class OnBookModelCreatedInterceptor : SaveChangesInterceptor
             result,
             cancellationToken: cancellationToken
         );
+    }
+
+    private async Task MapAuthorsAsync(
+        DbContext dbContext,
+        BookModel book,
+        CancellationToken cancellationToken
+    )
+    {
+        var missingAuthorIds = new List<long>();
+        foreach (var authorId in book.Publication.AuthorsIds)
+        {
+            var author = await dbContext.FindAsync<AuthorModel>([authorId], cancellationToken);
+            if (author is null)
+            {
+                missingAuthorIds.Add(authorId);
+                continue;
+            }
+
+            // Join Author and Authors via AuthorsBooks join table.
+            book.Authors.Add(author);
+        }
+
+        if (missingAuthorIds.Count > 0)
+        {
+            throw new InvalidOperationException(
+                $"The {nameof(Author)}s with following Ids do not exist: [{string.Join(';', missingAuthorIds)}]"
+            );
+        }
     }
 }
