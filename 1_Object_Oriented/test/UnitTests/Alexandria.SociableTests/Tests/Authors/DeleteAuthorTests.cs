f7f8a185db34f5ca0ab992b2fe74c1a8ab9f3b03
Functario <166994080+functario@users.noreply.github.com>
2025-03-31 09:55:01 -0400

    Move async extensions

--- 1_Object_Oriented/test/UnitTests/Alexandria.SociableTests/Tests/Authors/DeleteAuthorTests.cs
+++ 1_Object_Oriented/test/UnitTests/Alexandria.SociableTests/Tests/Authors/DeleteAuthorTests.cs
@@ -74,15 +74,3 @@ public class DeleteAuthorTests
         await sut.VerifyHttpResponseAsync();
     }
 }
-
-internal static class AsyncEnumerableExtensions
-{
-    public static async IAsyncEnumerable<T> ToAsyncEnumerable<T>(this IEnumerable<T> source)
-    {
-        foreach (var item in source)
-        {
-            yield return item;
-            await Task.Yield(); // Ensures it behaves as an async stream
-        }
-    }
-}
